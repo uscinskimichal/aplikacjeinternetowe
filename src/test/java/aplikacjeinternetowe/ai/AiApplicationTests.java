e63fc46bcee532ffc77ac501301cdb42a17e445a
Michał <uscinskimichal@gmail.com>
2019-10-25 23:19:07 +0200

    initial commit

---
+++ src/test/java/aplikacjeinternetowe/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package aplikacjeinternetowe.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
